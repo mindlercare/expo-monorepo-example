a4271c46db3568eaf613859f68fccc9d71399225
Piet Groot Kormelink <grop@me.com>
2025-02-26 22:09:37 +0100

    small fixes and config

--- packages/feature-home/src/HomeMessage.tsx
+++ packages/feature-home/src/HomeMessage.tsx
@@ -5,6 +5,6 @@ type HomeMessageProps = ComponentProps<typeof Paragraph>;
 
 export const HomeMessage = (props: HomeMessageProps) => (
   <Paragraph {...props}>
-    Hello from an <Strong>Expo monorepo</Strong>!
+    Hello FROM an <Strong>Expo monorepo</Strong>!
   </Paragraph>
 );
